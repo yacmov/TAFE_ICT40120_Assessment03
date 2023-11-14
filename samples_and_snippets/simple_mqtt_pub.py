952c61ec25a507c2b8d3a345d186aa2a0536cba6
Cedric <yacmov@gmail.com>
2023-11-15 00:27:24 +0800

    # add Basic app frame

--- samples_and_snippets/simple_mqtt_pub.py
+++ samples_and_snippets/simple_mqtt_pub.py
@@ -1,8 +1,11 @@
 import paho.mqtt.client as paho
+import time
 
-BROKER, PORT = "localhost", 1883
 
-client = paho.Client()
-client.connect(BROKER, PORT)
-client.publish("lot/sensor", "Hello from Python")
+
+def mqtt_broker(msg):
+    BROKER, PORT = "localhost", 1883
+    client = paho.Client()
+    client.connect(BROKER, PORT)
+    client.publish("lot/sensor", msg)
 
--- samples_and_snippets/simple_mqtt_sub.py
+++ samples_and_snippets/simple_mqtt_sub.py
@@ -1,10 +1,12 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import paho.mqtt.client as paho
 from paho.mqtt.client import MQTTMessage
 
 BROKER, PORT = "localhost", 1883
 
 def on_message(client, userdata, msg):
-    print(f'Received {msg.payload.decode()}')
+    print(f'{msg.payload.decode()}')
 
 client = paho.Client()
 client.on_message = on_message
